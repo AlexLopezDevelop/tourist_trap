7d6d92236f28ba1657273f9ac9cee6782b27b533
AlexLopezDevelop <alex_lopez.es@icloud.com>
2020-12-11 14:51:04 +0100

    added: navigation bar

---
+++ lib/src/pages/map_overview.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MapOverview extends StatefulWidget {
+  _MapOverview createState() => new _MapOverview();
+}
+
+class _MapOverview extends State<MapOverview> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
---
+++ lib/src/pages/navigator.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tourist_trap/src/pages/map_overview.dart';
+
+class NavigatorController extends StatefulWidget {
+  NavigatorController({Key key}) : super(key: key);
+
+  _NavigatorController createState() => new _NavigatorController();
+}
+
+class _NavigatorController extends State<NavigatorController> {
+  int currentPage = 0;
+  List<Widget> tabs = <Widget>[
+    //CameraScreen(widget.cameras),
+    MapOverview(),
+    Container(
+      color: Colors.red,
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: tabs[currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (int index) {
+          setState(() {
+            this.currentPage = index;
+          });
+          //_navigateToScreens(index);
+        },
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            label: "Puntos",
+            icon: Container(
+              width: 30,
+              height: 30,
+              child: SvgPicture.asset(
+                currentPage == 0
+                    ? "assets/icons/map-icon-selected.svg"
+                    : "assets/icons/map-icon.svg",
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "Más información",
+            icon: Container(
+              width: 30,
+              height: 30,
+              child: SvgPicture.asset(
+                currentPage == 1
+                    ? "assets/icons/info-icon-selected.svg"
+                    : "assets/icons/info-icon.svg",
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
